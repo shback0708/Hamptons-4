121b4434d25de09ef7ba9e3113be0664e22fcf09
dyuan0226 <33184700+dyuan0226@users.noreply.github.com>
2018-07-15 22:15:51 -0400

    broke up files into categories

---
+++ Basketball Analytics/game.py
@@ -0,0 +1,110 @@
+#Holds the game class, used to keep track of data within a specific game
+from player import *
+
+class Game(object):
+    def __init__(self, gameID, team1, team2):
+        self.ID = gameID
+        self.team1, self.team2 = team1, team2
+        self.inFreeThrow = False #variable to check how substitutions affect plus/minus within free throws
+        self.queuedSubs = set()
+        self.playersAppeared = dict() #maps playerID to player object
+
+    #for debugging purposes
+    def __repr__(self):
+        return self.ID
+
+    def __hash__(self):
+        return hash(self.ID)
+
+    def __eq__(self, other):
+        return (isinstance(other, Game) and (self.ID == other.ID))
+
+    #creates a player and adds him to self.playersAppeared
+    def createPlayer(self, PID, team):
+        if PID in self.playersAppeared:
+            return
+        self.playersAppeared[PID] = Player(PID, self.ID, team)
+
+    #updates lineups at the start of periods
+    def updateLineup(self, lineups, period):
+        team1Lineup = lineups[self.team1][period]
+        team2Lineup = lineups[self.team2][period]
+        lineup = team1Lineup.union(team2Lineup)
+
+        #starts by resetting all current players to not on the court
+        for player in self.playersAppeared:
+            self.playersAppeared[player].onCourt = False
+
+        #next two loops used to create players that haven't appeared in the game yet
+        for player in team1Lineup:
+            self.createPlayer(player, self.team1)
+        for player in team2Lineup:
+            self.createPlayer(player, self.team2)
+
+        #marks everyone subbed in at the beginning of the period as on the court
+        for person in lineup:
+            self.playersAppeared[person].onCourt = True
+
+    #handles substitutions
+    def substitute(self, playerOut, playerIn):
+        team = self.playersAppeared[playerOut].team #makes sure the two players are on the same team
+        self.createPlayer(playerIn, team) #player objects are created for players that haven't appeared yet
+        self.playersAppeared[playerOut].onCourt = False
+        self.playersAppeared[playerIn].onCourt = True
+
+    #handles queued substitutions after free throws
+    def doQueuedSubs(self):
+        for sub in self.queuedSubs: #sub: (playerIn, playerOut)
+            self.substitute(sub[0], sub[1])
+        self.queuedSubs = set()
+
+    #handles updating players' plus/minus's when points are scored
+    def updatePM(self, points, scorer):
+        team = self.playersAppeared[scorer].team
+
+        for player in self.playersAppeared:
+            self.playersAppeared[player].updatePM(points, team) #updated only for players that are on the court
+
+    #for debugging, counts number of player with onCourt == True (total, team1, team2)
+    def countOnCourt(self):
+        count = 0
+        t1count = 0
+        t2count = 0
+
+        for player in self.playersAppeared:
+
+            if self.playersAppeared[player].onCourt:
+                count += 1
+
+                if self.playersAppeared[player].team == self.team1:
+                    t1count += 1
+                if self.playersAppeared[player].team == self.team2:
+                    t2count += 1
+
+        return count, t1count, t2count
+
+    #again for debugging, checks the sum of all the players' plus/minus's in the game
+    #should be 0 at all times, since any action should result in a net of 0 total plus/minus
+    def totalPM(self):
+        total = 0
+
+        for player in self.playersAppeared:
+            total += self.playersAppeared[player].pm
+
+        return total
+
+    #again for debugging, to see which teams players are on the court
+    def checkLineups(self):
+        lineups = {self.team1:set(),self.team2:set()}
+
+        for player in self.playersAppeared:
+
+            if self.playersAppeared[player].onCourt:
+
+                if self.playersAppeared[player].team == self.team1:
+                    lineups[self.team1].add(self.playersAppeared[player])
+
+                elif self.playersAppeared[player].team == self.team2:
+                    lineups[self.team2].add(self.playersAppeared[player])
+
+        return lineups
---
+++ Basketball Analytics/player.py
@@ -0,0 +1,29 @@
+#Holds the player class, used to keep track of data for a player
+
+class Player(object):
+    def __init__(self, personID, gameID, team, onCourt=True, pm=0):
+        self.PID = personID
+        self.game = gameID
+        self.team = team
+        self.onCourt = onCourt
+        self.pm = pm
+
+    #for debugging purposes
+    def __repr__(self):
+        return self.PID
+
+    def __hash__(self):
+        return hash((self.PID, self.game))
+
+    def __eq__(self, other):
+        return (isinstance(other, Player) and (self.PID == other.PID) and (self.game == other.game))
+
+    #updates a player's plus/minus based on how many points were scored and which team scored
+    def updatePM(self, points, team):
+        if self.onCourt == False:
+            pass
+        else:
+            add = 1
+            if self.team != team:
+                add = -1
+            self.pm += add*points
